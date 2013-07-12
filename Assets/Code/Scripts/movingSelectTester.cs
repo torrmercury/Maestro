19cd3cd08ad8b1a22efd3309e4725eb679d868fe
Matt <matt.chen312@gmail.com>
2013-07-12 17:59:51 -0400

    Fixed Test Selector Movement
    CORINNE DID THIS DON'T BE FOOLED

--- Assets/Code/Scripts/movingSelectTester.cs
+++ Assets/Code/Scripts/movingSelectTester.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class movingSelectTester : MonoBehaviour {
-    int speed = 10;
-    float moveChar = 0.0f;
+//    int speed = 10;
+//    float moveChar = 0.0f;
 	// Use this for initialization
 	void Start () {
         
@@ -11,7 +11,27 @@ public class movingSelectTester : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        moveChar = Input.GetAxis("Vertical") * speed;
-        transform.position = new Vector3(0, moveChar, 0);
+        //moveChar = Input.GetAxis("Vertical") * speed;
+        //transform.position = new Vector3(0, moveChar * Time.deltaTime, 0);
+		
+		if (Input.GetKeyDown (KeyCode.UpArrow)){
+			Debug.Log ("Up Arrow was Pressed");
+			transform.position += new Vector3(0f, 10f, 0f);
+			
+		}else if (Input.GetKeyDown (KeyCode.DownArrow)){
+			Debug.Log ("Down Arrow was Pressed");
+			transform.position += new Vector3(0f, -10f, 0f);
+			
+		}
+		
+		if (Input.GetKeyDown (KeyCode.Space)){
+			Debug.Log ("Space was Pressed");
+			//transform.position += new Vector3(0f, 10f, 0f);
+			
+			
+		}
+		
+		
+		
 	}
 }
