810ecd41e2a97eef4ba75dceb72b0568e7cb99d1
Matt <matt.chen312@gmail.com>
2013-07-12 18:52:57 -0400

    Added constant forward motion to testSelector
    CORINNE AGAIN, BUT YOU ARE WISE TO THIS NOW
    Also added the beginnings of looping mechanics, but this is unfinished
    and commented out

--- Assets/Code/Scripts/movingSelectTester.cs
+++ Assets/Code/Scripts/movingSelectTester.cs
@@ -5,7 +5,11 @@ public class movingSelectTester : MonoBehaviour {
 //    int speed = 10;
 //    float moveChar = 0.0f;
 	
-	public Transform note;
+	public Transform note; //Assigned in inspector
+	public Transform startpoint; //Assigned in inspector
+	public Transform endpoint; //Assigned in inspector
+	
+	float tempo = 10; //tempo tells the testSelector how quickly it should move forward
 	
 	// Use this for initialization
 	void Start () {
@@ -14,9 +18,26 @@ public class movingSelectTester : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //moveChar = Input.GetAxis("Vertical") * speed;
-        //transform.position = new Vector3(0, moveChar * Time.deltaTime, 0);
 		
+		//Moves the testSelector back to the beginning of the bar staff once it hits the end
+		/*
+		 * This is currently buggy, a better solution would probably be to work with collision mechanics
+		 * I'll look into that once I've gone through the documentation a bit 
+		 * 
+		   if (gameObject.transform.position.x == endpoint.position.x){
+		
+			transfrom.postion.x = startpoint.position.x; 
+			
+			}
+		 */
+		
+
+		
+		//Moves the testSelector at a constant rate forward
+		transform.position += new Vector3(tempo *Time.deltaTime, 0f,0f);
+		
+		
+		//Moves the testSelector up and down on key presses
 		if (Input.GetKeyDown (KeyCode.UpArrow)){
 			Debug.Log ("Up Arrow was Pressed");
 			transform.position += new Vector3(0f, 10f, 0f);
@@ -27,10 +48,15 @@ public class movingSelectTester : MonoBehaviour {
 			
 		}
 		
+		//Places a note in front of the testSelector when the spacebar is hit
 		if (Input.GetKeyDown (KeyCode.Space)){
 			Debug.Log ("Space was Pressed");
 			
 			Instantiate(note, gameObject.transform.position + new Vector3(25f,0f,0f), Quaternion.identity);
+			
+			
+			
+			//The rest of this junk down here is probably not needed, but keep it around for reference
 			
 			//note = new GameObject("T"); //prefab or set in workspace?
 			//var meshFilter = gameObject.AddComponent<MeshFilter>();
